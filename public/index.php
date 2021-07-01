51ac2558a14d0a108a86da1cd2eea3afae7a59e7
root <dmitry.voskoboynikoff@gmail.com>
2021-07-01 12:28:53 +0300

    portion of code

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ $path = '..';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
 try {
+    /** Configuration */
     $configuration = new Framework\Configuration(array(
         "type" => "ini"
     ));
@@ -15,12 +16,21 @@ try {
     $configuration = $configuration->initialize();
     $configuration->parse(APP_PATH . '/application/configuration/database');
 
+    /**  Cache */
     $cache = new Framework\Cache(array(
        "type" => "memcached"
     ));
     $cache = $cache->initialize();
+    $cache->connect();
 
-} catch (Exception $e)
+    $friends = $cache->get("friends.1");
+    if (empty($friends))
+    {
+        $cache->set("friends.1", "testFriends");
+    }
+
+}
+catch (Exception $e)
 {
     print_r($e);
 }
